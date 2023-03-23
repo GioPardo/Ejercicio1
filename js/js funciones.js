e22c5133efc8c59fbb91ac48a25c0c2ae7a4f765
GioPardo <giopardo29@gmail.com>
2023-03-23 16:26:56 -0500

    Ejercicio cajero y flexbox

---
+++ js/js funciones.js
@@ -0,0 +1,19 @@
+let numero;
+
+numero = parseInt(prompt("numero 1"));
+numero2 = parseInt(prompt("numero 2"));
+numero3 = parseInt(prompt("numero 3"));
+numero4 = parseInt(prompt("numero 4"));
+
+function multiplicacion(){
+
+    let a = numero2 - numero;
+    a = numero2 + numero
+
+    console.log (a);
+
+}
+
+multiplicacion(multiplicacion);
+
+console.log(numero, numero2, numero3, numero4);
